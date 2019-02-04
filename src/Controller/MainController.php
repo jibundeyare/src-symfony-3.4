eaffaf87a0efa26bc6a996009dec91fab58b1765
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2019-02-04 02:02:26 +0100

    Add code and doc

---
+++ src/Controller/MainController.php
@@ -0,0 +1,47 @@
+<?php
+// src/Controller/MainController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Method;
+
+/**
+ * @Route("/")
+ */
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="main_index")
+     */
+    public function index(Request $request)
+    {
+        return $this->render('main/index.html.twig', [
+            // ...
+        ]);
+    }
+
+    /**
+     * @Route("/hello/{name}", name="main_hello")
+     */
+    public function hello(Request $request, $name)
+    {
+        $greeting = "Hello {$name}!";
+
+        return $this->render('main/hello.html.twig', [
+            'greeting' => $greeting,
+        ]);
+    }
+
+    /**
+     * @Route("/secured", name="main_secured")
+     */
+    public function secured(Request $request)
+    {
+        return $this->render('main/secured.html.twig', [
+            // ...
+        ]);
+    }
+}
---
+++ src/Entity/Student.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StudentRepository")
+ */
+class Student
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $birthdate;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $gender;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(?\DateTimeInterface $birthdate): self
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    public function getGender(): ?int
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?int $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+}
